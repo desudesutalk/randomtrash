682f69f1592e9f9632b8e69cb7661076c5ee4d81
desudesutalk <desudesutalk@users.noreply.github.com>
2015-11-27 12:43:56 +0300

    Update tripwars.user.js

--- tripwars/tripwars.user.js
+++ tripwars/tripwars.user.js
@@ -102,7 +102,7 @@ function parsePostResults(p){
 		m = file.textContent.match(/\d+\-([0-9a-f]+)\./);
 		rnd = parseInt(m[1], 16);
 	}
-		
+
 	trip = trip.textContent.substring(0,12);
 	name = name.textContent;
 
@@ -117,7 +117,7 @@ function parsePostResults(p){
 	}
 	
 	for (i = 0; i < spoils.length; i++) {
-		m = spoils[i].textContent.match(/^([astf])(:([a-z0-9а-я\-\s]+))?:(!{1,2}.+)$/i);
+		m = spoils[i].textContent.match(/^([astf])(:([a-z0-9а-я\-\s]{0,30}))?:(!{1,2}.+)$/i);
 		if(!m) continue;
 		if(m[4] == trip) break;
 
@@ -161,9 +161,8 @@ function parsePostResults(p){
 		m = refs[i].textContent.match(/>>(\d+)/);
 		if(!m) continue;
 		t = m[1];
-		if(t == trip) continue;
 
-		if(!tgPostHits[t] || tgPostHits[t].trip == trip || tgPostHits[t].hits[trip]) continue;
+		if(!tgPostHits[t] || tgPostHits[t].from == trip || tgPostHits[t].hits[trip]) continue;
 		tgPostHits[t].hits[trip] = true;
 		tgStats[tgPostHits[t].from].energy += 5;
 	}
@@ -190,7 +189,7 @@ function initTripGame(){
             $('form textarea#body').val($('form textarea#body').val() + '\n[h]S:'+e.target.parentNode.parentNode.dataset.trip+'[/h]');
         }
         if(e.target.textContent == 'T'){
-            $('form textarea#body').val($('form textarea#body').val() + '\n[h]T:'+prompt('Звание: ')+':'+e.target.parentNode.parentNode.dataset.trip+'[/h]');
+            $('form textarea#body').val($('form textarea#body').val() + '\n[h]T:'+prompt('Звание: ').substring(0,30)+':'+e.target.parentNode.parentNode.dataset.trip+'[/h]');
         }
     });
 }
@@ -208,7 +207,7 @@ function renderTripGame(){
 	  return b.energy - a.energy;
 	});
 
-	$('#tripwars').empty();
+	$('#twContent').empty();
 
 	for (var i = 0; i < pleers.length; i++) {
 		if(!tgStats[pleers[i].trip].prev){
@@ -238,7 +237,7 @@ function renderTripGame(){
 			shkvarki = '<span class="fr"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwBAMAAAClLOS0AAAAFVBMVEU0AAAAAAAAEf8/NiMtX2PueUL///+0Rm5gAAAAAXRSTlMAQObYZgAAARhJREFUOMut0luqAjEMBuCOl/cJ2ncpzPuR0AUIbqCGLsD9b+IkvU0zdR4Eg4Lm88+0QWNKTfyC2YwF8wTwQaQrtQuD1P4wrAHsTNpGdgG+hel3AL+DZ60OJv5wavB8rPuCdVKO1FR/1Kt6EEuUCv0R8jBL6K1F9JFQne3Mv7dWUoR47yLn+Oa2vBlQQQwxFyLd1agNlFuuQBgSmAa2gLcC8whBEusSZVIahiFgv13uhgzRaiCAS06QBr4XL0QSqEAujCinvWhIfY7w9fBPA3HI85tU3xzTGM8pMhvwebN+gFJxBzj3GfgmNwVLC7wUuNgCznX9w5IOK7t6OVcjB+fcIksPBao4Kfn7JEjfbgpiDShYCqQv+fH/e2h063llRdsAAAAASUVORK5CYII=" style="width:25px;" title="Чак Норрис" alt="Чак Норрис"></span>';
 		}
 
-		$('#tripwars').append('<div data-trip="'+pleers[i].trip+'"' + (i == 0? ' style="font-size:16px"':'') + '>'+ 
+		$('#twContent').append('<div data-trip="'+pleers[i].trip+'"' + (i == 0? ' style="font-size:16px"':'') + '>'+ 
 			(pleers[i].title? '<em>'+pleers[i].title.title+'</em> ' : '') +
 			'<strong>' + pleers[i].name +'</strong><span style="color: #228854;">'+pleers[i].trip+'</span>'+
 			'<span class="fr badge"><strong>'+pleers[i].energy+'</strong></span>'+
@@ -266,8 +265,9 @@ $(function(){
 			renderTripGame();
 			return true;
 		});
-		$('body').append('<div id="tripwars"></div>');
-		$('head').append('<style type="text/css">  #tripwars { max-height: 90%; overflow-y: auto; min-width: 300px; position: fixed; top: 40px; right: 30px; background: #fff; padding: 5px; font-size: 12px; border-radius: 3px; box-shadow: 0px 0px 10px rgba(0,0,0,0.25); font-family: \'Roboto\', Arial, sans-serif; counter-reset: pstn; } #tripwars div:before { counter-increment: pstn; content: counter(pstn) ": "; } #tripwars div { padding: 5px; border-bottom: 1px solid #eee; position: relative; } #tripwars span.fr{ float: right; margin-left: 5px; } #tripwars div:hover span.fr{ visibility: hidden; } #tripwars div:hover span.ctrls{ display: block; position: absolute; right: 0; top: 0; margin-top: auto; margin-bottom: auto; bottom: 0; height: 12px; } #tripwars div span.ctrls{ display: none; } #tripwars span.badge{ color: white; background: #3db; padding: 3px; border-radius: 10px; } #tripwars br{ clear: both; } </style>');
+		$('body').append('<div id="tripwars"><span id="twCollapser">#</span> <span id="odometer" style="float: right;"></span><div id="twContent"></div></div>');
+		$('head').append('<style type="text/css">   #tripwars { max-height: 90%; overflow-y: auto; min-width: 300px; position: fixed; top: 15px; right: 30px; background: #fff; padding: 5px; font-size: 12px; border-radius: 3px; box-shadow: 0px 0px 10px rgba(0,0,0,0.25); counter-reset: pstn; } #twContent div:before { counter-increment: pstn; content: counter(pstn) ": "; } #twContent div { padding: 5px; border-bottom: 1px solid #eee; position: relative; } #tripwars span.fr{ float: right; margin-left: 5px; } #twContent div:hover span.fr{ visibility: hidden; } #twContent div:hover span.ctrls{ display: block; position: absolute; right: 0; top: 0; margin-top: auto; margin-bottom: auto; bottom: 0; height: 12px; } #twContent div span.ctrls{ display: none; } #tripwars span.badge{ color: white; background: #3db; padding: 3px; border-radius: 10px; } #tripwars br{ clear: both; } .twShowLess div { display:none; } .twShowLess .first-child { display:block; } #twCollapser {cursor: pointer;}</style>');
+		$('#twCollapser').on('click', function(){$('#twContent').toggleClass('twShowLess')})
 		initTripGame();
 		
 		// Odometer
@@ -275,7 +275,6 @@ $(function(){
 			var spd = odometer();
 			$('#odometer').text('Speed: ' + spd.speed + 'pph (' + spd.percent + '%) Autosage in: ' + spd.timeToBL);
 		}
-		$('body').append('<div id="odometer" style="position: fixed; top: 10px; right: 30px; background: #fff; padding: 5px; font-size: 10px; border-radius: 3px; box-shadow: 0px 0px 10px rgba(0,0,0,0.25);"></div>');
 		setInterval(updateOdometer, 15000);
 		updateOdometer();
         $(document).on('new_post', updateOdometer);
