08a8fa5be4e87fe483cf83790991f40062a1bf1f
bokufag <souseiseki.desu@yandex.ru>
2015-12-18 12:40:52 +0300

    New config option

--- tripwars/tripwars.meta.js
+++ tripwars/tripwars.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SynchTripWars
 // @namespace    udp://SynchTripWars/*
-// @version      0.0.49
+// @version      0.0.50
 // @description  post something useful
 // @include      *://*syn-ch.com/*
 // @include      *://*syn-ch.org/*
