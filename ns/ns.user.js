82fde8dc22fe311863d32f81c99400b26dbde858
bokufag <souseiseki.desu@yandex.ru>
2016-01-30 20:18:49 +0300

    Fix typo

--- ns/ns.user.js
+++ ns/ns.user.js
@@ -719,7 +719,7 @@ Zepto(function($){
 
 	if(/^\/reply\//.test(document.location.pathname)){
 		$('#sendbtn').parent().prepend('<div style="display: block; float: right; margin-left: 20px;"><button id="imgopen">Пришакалить картинку</button><input type="file" style="display: none;" id="imgfile">'
-			+ '<br>Какчестов: <input id="imgq" type="range" min="0" max="100" step="1" value="50" style="width: 100px;"> <span id="qtxt">50</span>'
+			+ '<br>Какчество: <input id="imgq" type="range" min="0" max="100" step="1" value="50" style="width: 100px;"> <span id="qtxt">50</span>'
 			+ '<br>Коликчество: <input id="imgs" type="range" min="0" max="16384" step="256" value="16384" style="width: 100px;"> <span id="stxt">16384</span><br>'
 			+ (doWebp ? '<label>WebP <input type="checkbox" value="webp" id="do_webp" checked></label><br>': '')
 			+ '<div id="previewdiv"></div></div>');
